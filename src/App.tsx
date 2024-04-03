3c85fdec2f5f3bb587c129e8786d7db68ade7393
lucasdaher <contato@lucasdaher.com>
2024-04-03 17:04:27 -0300

    chore: change configuration files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
 import Presentation from "./components/Presentation/presentation";
 import Header from "./components/header";
+import Skills from "./components/Categories/skills";
 
 function App() {
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  const handleIsDark = () => {
+    setIsDark(!isDark);
+  };
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDark]);
+
   return (
     <div className="app">
-      <Header />
+      <Header isDark={isDark} handleIsDark={handleIsDark} />
 
       <main className="mt-24">
         <Presentation
+          isDark={isDark}
           title="Me chamo Lucas Daher"
-          desc="Sou apaixonado por tecnologia e por arte. Atuo como desenvolvedor web focado em front-end a pouco menos de 1 ano."
+          desc="Com quase um ano de experiência como desenvolvedor front-end, estou sempre buscando novos desafios e aprimorando minhas habilidades para criar interfaces incríveis e funcionais."
         />
+        <Skills isDark={isDark} />
       </main>
     </div>
   );
