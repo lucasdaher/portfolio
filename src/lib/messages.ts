e923f283ecab77c9e3c1b4d39cff633f8819e1f7
lucasdaher <contato@lucasdaher.com>
2024-07-21 22:49:59 -0300

    add messages configuration.

---
+++ src/lib/messages.ts
@@ -0,0 +1,14 @@
+export const messages = {
+  pt: {
+    navItem01: "Sobre",
+    navItem02: "Projetos",
+    navItem03: "Contato",
+    languageAlert: "A alteração de idiomas está em desenvolvimento...",
+  },
+  en: {
+    navItem01: "About",
+    navItem02: "Projects",
+    navItem03: "Contact",
+    languageAlert: "Language switching is under development...",
+  },
+};
