839e5187c059c6b6a640c9b21fe460be97812fe4
lucasdaher <contato@lucasdaher.com>
2024-07-27 03:28:36 -0300

    change main page to maintenance.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import Error from "./views/error/Error";
-import Home from "./views/Home";
+// import Home from "./views/Home";
 
 import { createBrowserRouter } from "react-router-dom";
+import Maintenance from "./views/maintanance/Maintanance";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Maintenance />,
+    // element: <Home />,
     errorElement: <Error />,
   },
 ]);
