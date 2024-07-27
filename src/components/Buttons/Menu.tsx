847fae29ea8e38d477563ddb76a53c68a1481477
lucasdaher <contato@lucasdaher.com>
2024-07-27 03:15:30 -0300

    change update to disabled.

--- src/components/Buttons/Menu.tsx
+++ src/components/Buttons/Menu.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { AlignRight, History, Moon, Sun } from "lucide-react";
 
 interface MenuProps {
@@ -43,17 +43,17 @@ export default function Menu({ iconStyle, handleIsDark, isDark }: MenuProps) {
               className="text-white cursor-pointer"
               onClick={handleIsDark}
             >
-              {isDark === false ? (
-                <Sun className={`mr-2 h-4 w-4 text-black-gray`} />
+              {isDark === true ? (
+                <Sun className={`mr-2 h-4 w-4 text-white`} />
               ) : (
-                <Moon className={`mr-2 h-4 w-4 text-white`} />
+                <Moon className={`mr-2 h-4 w-4 text-black-gray`} />
               )}
               <span
                 className={`${
-                  isDark === false ? "text-black-gray" : "text-white"
+                  isDark === true ? "text-white" : "text-black-gray"
                 }`}
               >
-                Alterar modo
+                {isDark === true ? "Modo claro" : "Modo escuro"}
               </span>
               <span
                 className={`ml-2 bg-blue-500 rounded-md font-sora font-bold text-xs px-1 animate-rotate text-white`}
@@ -63,23 +63,27 @@ export default function Menu({ iconStyle, handleIsDark, isDark }: MenuProps) {
             </DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator className="bg-gray/20" />
-          <DropdownMenuGroup className="hover:bg-gray/20 transition-all duration-200 ease-in-out rounded-md">
-            <Link to={`/updates`}>
-              <DropdownMenuItem className="text-white cursor-pointer">
-                <History
-                  className={`mr-2 h-4 w-4 ${
-                    isDark === false ? "text-black-gray" : "text-white"
-                  }`}
-                />
-                <span
-                  className={`${
-                    isDark === false ? "text-black-gray" : "text-white"
-                  }`}
-                >
-                  Últimas atualizações
-                </span>
-              </DropdownMenuItem>
-            </Link>
+          {/* <DropdownMenuGroup className="hover:bg-gray/20 transition-all duration-200 ease-in-out rounded-md"> */}
+          <DropdownMenuGroup>
+            {/* <Link to={`/updates`}> */}
+            <DropdownMenuItem
+              className="text-white cursor-pointer"
+              disabled={true}
+            >
+              <History
+                className={`mr-2 h-4 w-4 ${
+                  isDark === false ? "text-black-gray" : "text-white"
+                }`}
+              />
+              <span
+                className={`${
+                  isDark === false ? "text-black-gray" : "text-white"
+                }`}
+              >
+                Últimas atualizações
+              </span>
+            </DropdownMenuItem>
+            {/* </Link> */}
           </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
