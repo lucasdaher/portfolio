6de8ac73a205925f02a5bec80e5a4e7127f4fe4c
lucasdaher <contato@lucasdaher.com>
2024-07-27 03:33:03 -0300

    maintenance removed

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,14 @@ import Error from "./views/error/Error";
 
 import { createBrowserRouter } from "react-router-dom";
 
-// import Home from "./views/Home";
-import Maintenance from "./views/maintenance/Maintenance"; // Comment this to active maintenance
+import Home from "./views/Home";
+// import Maintenance from "./views/maintenance/Maintenance"; // Comment this to active maintenance
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Maintenance />,
-    // element: <Home />,
+    // element: <Maintenance />,
+    element: <Home />,
     errorElement: <Error />,
   },
 ]);
