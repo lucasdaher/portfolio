94763903289347afc3d66ee3004eb19a6e250c4c
lucasdaher <contato@lucasdaher.com>
2024-08-01 05:39:30 -0300

    add language switch to ptbr and en.

--- src/locales/languages.ts
+++ src/locales/languages.ts
@@ -1,6 +1,17 @@
 export type Language = "en" | "ptbr";
 export interface Messages {
-  resumeBtnDownload: string;
   presentationTitle: string;
   presentationTitleDesc: string;
+  langs: { ptbr: string; en: string; es: string };
+  alerts: { tipBox: string };
+  header: { about: string; skills: string; projects: string; contact: string };
+  buttons: {
+    resumeBtnDownload: string;
+    switchModeToLight: string;
+    switchModeToDark: string;
+    lastUpdates: string;
+    new: string;
+    soon: string;
+  };
+  buttonTitles: { resumeBtn: string };
 }
