17cb36d3f09150e9b7ff7dbbd3a1612492f604c9
lucasdaher <contato@lucasdaher.com>
2024-08-01 05:40:36 -0300

    removed unused imports.

--- src/components/Home/Demonstration/Demonstration.tsx
+++ src/components/Home/Demonstration/Demonstration.tsx
@@ -1,13 +1,10 @@
 import MarqueeContent from "./MarqueeContent";
 
-import { useDarkMode } from "@/services/ThemeService";
-
 interface DemonstrationProps {
   id?: string;
 }
 
 export default function Demonstration({ id }: DemonstrationProps) {
-  const { dark } = useDarkMode();
   return (
     <section className="w-full mt-12" id={id}>
       <div className="w-full">
