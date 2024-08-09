6db73659f05e6a8ba1650009c26286c3a4614b5b
lucasdaher <contato@lucasdaher.com>
2024-08-08 21:44:59 -0300

    add filter to projects section.

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { ArrowRight, ListRestartIcon, Settings2Icon } from "lucide-react";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useLanguage } from "@/hooks/useLanguage";
@@ -24,6 +24,14 @@ interface ProjectsProps {
 export default function Projects({ id }: ProjectsProps) {
   const { messages } = useLanguage();
   const { dark } = useContext(ThemeContext);
+
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+
+  const filteredProjects = projects.filter(
+    (projeto) =>
+      !selectedTag || projeto.tags.some((tag) => tag.tag === selectedTag)
+  );
+
   return (
     <section className="bg-transparent mt-36 px-4" id={id}>
       <div className="max-w-[1440px] mx-auto w-full text-white">
@@ -62,54 +70,46 @@ export default function Projects({ id }: ProjectsProps) {
             </DropdownMenuLabel>
             <DropdownMenuSeparator className="bg-gray/20" />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <span className="cursor-pointer hover:text-white/70">
-                  Mais recentes
-                </span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span className="cursor-pointer hover:text-white/70">
-                  Mais antigos
-                </span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span className="cursor-pointer hover:text-white/70">
-                  Ordem Alfabética
-                </span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => setSelectedTag("Projeto Acadêmico")}
+              >
                 <span className="cursor-pointer hover:text-white/70">
                   Projeto Acadêmico
                 </span>
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => setSelectedTag("Lógica de Programação")}
+              >
                 <span className="cursor-pointer hover:text-white/70">
                   Lógica de Programação
                 </span>
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setSelectedTag("Front-End")}>
                 <span className="cursor-pointer hover:text-white/70">
                   Front-End
                 </span>
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setSelectedTag("Back-End")}>
                 <span className="cursor-pointer hover:text-white/70">
                   Back-End
                 </span>
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setSelectedTag("UX Design")}>
                 <span className="cursor-pointer hover:text-white/70">
                   UX Design
                 </span>
               </DropdownMenuItem>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setSelectedTag("Design")}>
                 <span className="cursor-pointer hover:text-white/70">
                   Design
                 </span>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator className="bg-gray/20" />
-            <DropdownMenuItem className="text-red-500 flex justify-center items-center">
+            <DropdownMenuItem
+              className="text-red-500 flex justify-center items-center"
+              onClick={() => setSelectedTag(null)}
+            >
               <ListRestartIcon className="w-5 h-5 mr-2" />
               <span className="leading-none h-auto">Resetar filtros</span>
             </DropdownMenuItem>
@@ -117,59 +117,78 @@ export default function Projects({ id }: ProjectsProps) {
         </DropdownMenu>
 
         {/* Implementar os cards com os projetos */}
-        <div className="grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 md:gap-x-4 md:gap-y-8 w-full max-w-none mt-12 items-start">
-          {projects.map((projeto) => (
-            <React.Fragment key={projeto.id}>
-              <div className="flex flex-col justify-between items-start gap-4 max-h-[750px] min-h-[748px]">
-                <picture className="shadow-projectCard rounded-lg w-full">
-                  {projeto.thumbnail}
-                </picture>
-                <div className="flex flex-col justify-center items-start gap-2">
-                  <h1
-                    className={`font-sora font-bold text-xl leading-none h-auto ${
-                      dark === true ? "text-white" : "text-black-gray"
-                    } transition-all duration-400 ease-in-out`}
-                  >
-                    {projeto.titulo}
-                  </h1>
-                  <p
-                    className={`font-sora font-normal text-base leading-normal min-h-16 ${
-                      dark === true ? "text-white/70" : "text-black-gray"
-                    }`}
-                  >
-                    {projeto.descricao}
-                  </p>
-                  <div className="tags flex flex-row justify-start items-center gap-2 mt-2">
-                    {projeto.tags.map((tag, index) => (
-                      <div
-                        className="bg-normal px-2 py-1 rounded-lg text-sm text-white"
-                        key={index}
-                      >
-                        {tag.tag}
-                      </div>
-                    ))}
-                  </div>
-                  <Link to={`${projeto.link}`}>
-                    <button
-                      className={`mt-6 border rounded-3xl px-4 py-2 ${
-                        dark === true
-                          ? "border-white group hover:bg-white hover:text-black-gray"
-                          : "border-black-gray group hover:bg-black-gray hover:text-white"
-                      } transition-all duration-200 ease-in-out`}
+        <div
+          className={`${
+            filteredProjects.length === 0
+              ? ""
+              : "grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 md:gap-x-4 md:gap-y-8"
+          } w-full max-w-none mt-12 items-start`}
+        >
+          {filteredProjects.length === 0 ? (
+            <div className="max-w-full">
+              <p
+                className={`${
+                  dark === true ? "text-white/80" : "text-black-gray"
+                } text-base font-sora font-normal text-left transition-all duration-400 ease-in-out`}
+              >
+                Nenhum projeto encontrado com as definições de filtro
+                informadas.
+              </p>
+            </div>
+          ) : (
+            filteredProjects.map((projeto) => (
+              <React.Fragment key={projeto.id}>
+                <div className="flex flex-col justify-between items-start gap-4 max-h-[750px] min-h-[748px]">
+                  <picture className="shadow-projectCard rounded-lg w-full">
+                    {projeto.thumbnail}
+                  </picture>
+                  <div className="flex flex-col justify-center items-start gap-2">
+                    <h1
+                      className={`font-sora font-bold text-xl leading-none h-auto ${
+                        dark === true ? "text-white" : "text-black-gray"
+                      } transition-all duration-400 ease-in-out`}
                     >
-                      <ArrowRight
-                        className={`transition-all duration-200 ease-in-out ${
+                      {projeto.titulo}
+                    </h1>
+                    <p
+                      className={`font-sora font-normal text-base leading-normal min-h-16 ${
+                        dark === true ? "text-white/70" : "text-black-gray"
+                      }`}
+                    >
+                      {projeto.descricao}
+                    </p>
+                    <div className="tags flex flex-row justify-start items-center gap-2 mt-2">
+                      {projeto.tags.map((tag, index) => (
+                        <div
+                          className="bg-normal px-2 py-1 rounded-lg text-sm text-white"
+                          key={index}
+                        >
+                          {tag.tag}
+                        </div>
+                      ))}
+                    </div>
+                    <Link to={`${projeto.link}`}>
+                      <button
+                        className={`mt-6 border rounded-3xl px-4 py-2 ${
                           dark === true
-                            ? "text-white group-hover:text-black-gray"
-                            : "text-black-gray group-hover:text-white"
-                        }`}
-                      />
-                    </button>
-                  </Link>
+                            ? "border-white group hover:bg-white hover:text-black-gray"
+                            : "border-black-gray group hover:bg-black-gray hover:text-white"
+                        } transition-all duration-200 ease-in-out`}
+                      >
+                        <ArrowRight
+                          className={`transition-all duration-200 ease-in-out ${
+                            dark === true
+                              ? "text-white group-hover:text-black-gray"
+                              : "text-black-gray group-hover:text-white"
+                          }`}
+                        />
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            </React.Fragment>
-          ))}
+              </React.Fragment>
+            ))
+          )}
         </div>
       </div>
     </section>
