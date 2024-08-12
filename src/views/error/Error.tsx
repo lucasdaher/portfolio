1811bbbd60e12fe659234030503c5d5660786c09
lucasdaher <contato@lucasdaher.com>
2024-08-11 23:16:26 -0300

    add translations to error page.

--- src/views/error/Error.tsx
+++ src/views/error/Error.tsx
@@ -16,20 +16,22 @@ export default function Error({}: ErrorProps) {
     <React.Fragment>
       <Header homePage={false} />
       <main className="flex flex-col justify-center items-center mt-36 px-4">
-        <h1 className="text-normal text-5xl font-sora font-bold">Oops!</h1>
+        <h1 className="text-normal text-5xl font-sora font-bold">
+          {messages.errorPage.title}
+        </h1>
         <p
           className={`${
             dark === true ? "text-white/80" : "text-black-gray"
           } text-lg font-sora font-normal mt-4 text-center transition-all duration-400 ease-in-out`}
         >
-          A página que você está procurando não foi encontrada.
+          {messages.errorPage.desc}
         </p>
         <Link
           to={`/`}
           className="mt-8 group hover:text-light-active transition-all duration-200 ease-in-out"
         >
           <button className="border border-normal rounded-full px-8 py-4 text-normal text-center text-sm md:text-base font-sora font-semibold md:font-normal group-hover:text-white group-hover:bg-normal transition-all duration-200 ease-in-out">
-            Clique para voltar à página principal
+            {messages.errorPage.buttons.returnToHome}
           </button>
         </Link>
         {/* Footer options */}
@@ -43,7 +45,7 @@ export default function Error({}: ErrorProps) {
             } transition-all duration-200 ease-in-out`}
           >
             <li className="text-center text-sm font-sora font-light">
-              Projetos
+              {messages.errorPage.buttons.projects}
             </li>
           </Link>
           <Link
@@ -55,7 +57,7 @@ export default function Error({}: ErrorProps) {
             } transition-all duration-200 ease-in-out`}
           >
             <li className="text-center text-sm font-sora font-light">
-              Habilidades
+              {messages.errorPage.buttons.skills}
             </li>
           </Link>
           <Link
@@ -67,11 +69,11 @@ export default function Error({}: ErrorProps) {
             } transition-all duration-200 ease-in-out`}
           >
             <li className="text-center text-sm font-sora font-light">
-              Contato
+              {messages.errorPage.buttons.contact}
             </li>
           </Link>
           <Link
-            to={`/`}
+            to={`/aboutme`}
             className={`${
               dark === true
                 ? "text-white/40 hover:text-white"
@@ -79,7 +81,7 @@ export default function Error({}: ErrorProps) {
             } transition-all duration-200 ease-in-out`}
           >
             <li className="text-center text-sm font-sora font-light">
-              Baixar Currículo
+              {messages.errorPage.buttons.aboutme}
             </li>
           </Link>
         </ul>
