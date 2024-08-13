4923d989f2530dfc85098bb429d23da431ea8239
lucasdaher <contato@lucasdaher.com>
2024-08-12 22:15:13 -0300

    add about page in routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,18 @@ import Error from "./views/error/Error";
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./views/Home";
+import AboutMe from "./views/aboutme/AboutMe";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     // element: <Maintenance />,
     element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/aboutme",
+    element: <AboutMe />,
     errorElement: <Error />,
   },
 ]);
